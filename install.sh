627a16c89cfc7fe1430df37ba71d60adc3e7edc1
fchrstou <filippos.christou@ikr.uni-stuttgart.de>
2024-06-28 09:54:56 +0200

    Make some app installation default

--- install.sh
+++ install.sh
@@ -10,39 +10,34 @@ ln -sf ~/configfiles/inputrc ~/.inputrc
 ln -sf ~/configfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/configfiles/nvim ~/.config/nvim
 
-if [[ `uname -n` == *"cnode"* ]] || [[ `uname -n` == "pc114" ]] ; then
-	# IKR devices
+# install juliaup
+curl -fsSL https://install.julialang.org | sh
 
-    # binaries folder
+WEZTERM_VERSION=20240203-110809-5046fc22
+WEZTERM_UBUNTU_VERSION=Ubuntu20.04
+WEZTERM_APPIMAGE=WezTerm-$WEZTERM_VERSION-$WEZTERM_UBUNTU_VERSION.AppImage
+# install wezterm
+curl -LO https://github.com/wez/wezterm/releases/download/$WEZTERM_VERSION/$WEZTERM_APPIMAGE
+chmod +x $WEZTERM_APPIMAGE
+./$WEZTERM_APPIMAGE --appimage-extract
+rm $WEZTERM_APPIMAGE
+mv ./squashfs-root $CHAPPS/apps/wezterm
+ln -sf $CHAPPS/apps/wezterm/usr/bin/wezterm $CHAPPS/bin/
 
-    # install juliaup
-    curl -fsSL https://install.julialang.org | sh
+# install nvim
+mkdir -p $CHAPPS/apps/nvim
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar -xz -C $CHAPPS/apps/nvim/ --strip-components=1
+ln -sf $CHAPPS/apps/nvim/bin/nvim $CHAPPS/bin/
 
-    WEZTERM_VERSION=20240203-110809-5046fc22
-    WEZTERM_UBUNTU_VERSION=Ubuntu20.04
-    WEZTERM_APPIMAGE=WezTerm-$WEZTERM_VERSION-$WEZTERM_UBUNTU_VERSION.AppImage
-    # install wezterm
-    curl -LO https://github.com/wez/wezterm/releases/download/$WEZTERM_VERSION/$WEZTERM_APPIMAGE
-    chmod +x $WEZTERM_APPIMAGE
-    ./$WEZTERM_APPIMAGE --appimage-extract
-    rm $WEZTERM_APPIMAGE
-    mv ./squashfs-root $CHAPPS/apps/wezterm
-    ln -sf $CHAPPS/apps/wezterm/usr/bin/wezterm $CHAPPS/bin/
+# cargo-bins in ~/.cargo/bin
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+cargo binstall ripgrep
 
-    # install nvim
-    mkdir -p $CHAPPS/apps/nvim
-    curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar -xz -C $CHAPPS/apps/nvim/ --strip-components=1
-    ln -sf $CHAPPS/apps/nvim/bin/nvim $CHAPPS/bin/
-
-    # cargo-bins in ~/.cargo/bin
-    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-    cargo binstall ripgrep
-
-else
+if [[ ! `uname -n` == *"cnode"* ]] && [[ ! `uname -n` == "pc114" ]] ; then
 	# other devices with sudo rights
 	echo "do nothing"
-    # install homebrew
-    
+	# install homebrew
+
     # install riggrep
 fi
 
